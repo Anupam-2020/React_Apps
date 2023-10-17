c6df1d75c2e2c2c48decd4f8e4b07cde542a3f31
Anupam-2020 <anupamanand.696@gmail.com>
2023-10-18 03:03:22 +0530

    added a todo app using vanilla typescript

---
+++ todo_app_typescript/src/main.ts
@@ -0,0 +1,77 @@
+interface Todo {
+  title: string,
+  isCompleted: boolean,
+  readonly _id: string
+}
+
+const todos: Todo[] = [];
+
+const todoContainer: HTMLElement = document.querySelector(".todoContainer") as HTMLDivElement;
+
+const todoInput: HTMLInputElement = document.getElementsByName("task")[0] as HTMLInputElement;
+
+const myForm: HTMLFormElement = document.getElementById("form") as HTMLFormElement;
+
+// console.log(todoInput.value)
+
+myForm.onsubmit = (e: SubmitEvent) => {
+  e.preventDefault();
+
+  const todo: Todo = {
+    title: todoInput.value,
+    isCompleted: false,
+    _id: String(Math.random()*1000)
+  }
+
+  todos.push(todo);
+  todoInput.value = "";
+  renderTodo(todos);
+  console.log(todos);
+};
+
+function generateTodoItem(todo: Todo) {
+
+  // Creating a parent div which holds all the other elements.
+  const todoDiv: HTMLDivElement = document.createElement("div");
+  todoDiv.className = "todoDiv";
+
+  // Creating p-tag for title
+  const todoPara: HTMLParagraphElement = document.createElement("p");
+  todoPara.textContent = todo.title
+
+  // Creating a checkbox
+  const checkbox: HTMLInputElement = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.className = "isCompleted";
+  checkbox.checked = todo.isCompleted;
+  checkbox.onchange = () => {
+    todoPara.className = checkbox.checked ? "textCut" : ""
+  }
+
+  // Creating div containing p and checkbox
+  const innerDiv: HTMLDivElement = document.createElement("div");
+  innerDiv.className = "innerDiv";
+
+  innerDiv.append(checkbox, todoPara);
+
+  // Creating delete button
+  const deleteBtn: HTMLButtonElement = document.createElement("button");
+  deleteBtn.innerText = "X";
+  deleteBtn.className = "deleteBtn";
+  deleteBtn.onclick = (): void => {
+    const idx: number = todos.findIndex((tod: Todo) => tod._id === todo._id);
+    todos.splice(idx, 1);
+    renderTodo(todos);
+  }
+
+  // Appending all tags to div.
+  todoDiv.append(innerDiv, deleteBtn);
+  todoContainer.append(todoDiv);
+}
+
+function renderTodo(todos: Todo[]) {
+  todoContainer.innerText = "";
+  todos.map((todo: Todo) => {
+    generateTodoItem(todo)
+  })
+}
